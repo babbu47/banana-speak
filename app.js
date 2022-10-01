ac32862edd14154ee690b1dd2b4fa5cc2589418a
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-02 00:40:01 +0530

    changes made

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 var btnt= document.querySelector("#btn-translate")
 var inputxt=document.querySelector("#txt-input")
 var outputdiv=document.querySelector("#output")
-var url="	https://api.funtranslations.com/translate/sith.json";
+var url="https://api.funtranslations.com/translate/minion.json";
 
 function constructurl(tt)
 {
-    return url+"?"+"text="+tt
+  return url +"?"+"text="+tt
 }
 
 
 function errorhandler(error)
 {
-    console.log("error occurd",error)
-    alert("something wrong with server ")
+    console.log("error occured",error);
+    alert("something wrong with server ");
 }
 
 function clickhandle()
 {
-    var textinput=inputtxt.value;
+    var textinput=inputxt.value;
     fetch(constructurl(textinput))
     .then(response=> response.json())
-.then(js=>{
-    var translatedtxt=json.contents.translated
-    outputdiv.innerText=translatedtxt})
+     .then(json=>
+        {
+    var translatedtxt=json.contents.translated;
+
+    outputdiv.innerText=translatedtxt;
+})
     .catch(errorhandler)
-}
+};
 btnt.addEventListener("click",clickhandle)
