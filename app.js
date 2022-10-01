95f6053abddd2c31961f5b8598ec8e1c0e3d95e0
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-01 14:43:25 +0530

    update  text

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var btnt= document.querySelector("#btn-translate")
-
+var inputxt=document.querySelector("txt-input")
 btnt.addEventListener("click",function clickhandle(){
     console.log("clicked!")
+    console.log("input".inputtxt.value)
 })
