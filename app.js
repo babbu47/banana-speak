680f46c455d65a587d1eaef9caecd6dc15b8276d
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-01 22:53:17 +0530

    url added

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var btnt= document.querySelector("#btn-translate")
 var inputxt=document.querySelector("#txt-input")
 var outputdiv=document.querySelector("#output")
+var url="	https://api.funtranslations.com/translate/sith.json";
 
-btnt.addEventListener("click",function clickhandle(){
-    console.log("clicked!")
-    console.log("input",inputxt.value)
-    outputdiv.innerText="output will appear here";
-})
+function constructurl(tt)
+{
+    return url+"?"+"text="+tt
+}
 
+
+function errorhandler(error)
+{
+    console.log("error occurd",error)
+    alert("something wrong with server ")
+}
+
+function clickhandle()
+{
+    var textinput=inputtxt.value;
+    fetch(constructurl(textinput))
+    .then(response=> response.json())
+.then(js=>{
+    var translatedtxt=json.contents.translated
+    outputdiv.innerText=translatedtxt})
+    .catch(errorhandler)
+}
+btnt.addEventListener("click",clickhandle)
