0717c6890ea0318a31b5e2951afeddffc73287e2
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-01 15:42:13 +0530

    inputoutput processsing done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btnt= document.querySelector("#btn-translate")
 var inputxt=document.querySelector("#txt-input")
 var outputdiv=document.querySelector("#output")
+
 btnt.addEventListener("click",function clickhandle(){
     console.log("clicked!")
-    console.log("input".inputxt.value)
+    console.log("input",inputxt.value)
+    outputdiv.innerText="output will appear here"
 })
 
